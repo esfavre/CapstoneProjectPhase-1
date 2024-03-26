6a33e141f6f33e7f1e47d2d6c0d283f45ff3dd31
Elena Favre <efavre@comcast.net>
2024-03-25 21:25:41 -0700

    navbar styling fixed, added client form

--- ClientManagementWebsite/client-management-app/src/app/client-form/client-form.component.ts
+++ ClientManagementWebsite/client-management-app/src/app/client-form/client-form.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-client-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './client-form.component.html',
   styleUrl: './client-form.component.css'
 })
-export class ClientFormComponent {
 
-}
+export class ClientFormComponent {
+  public fname = "";
+  public lname = "";
+  public email = "";
+  public email_preference = "";
+
+  onSubmit() {
+    console.log(`-----------------------------------------------`)
+    console.log(` First name: ${this.fname}`)
+    console.log(`  Last name: ${this.lname}`)
+    console.log(`      email: ${this.email}`)
+    console.log(`allow email: ${this.email_preference}`)
+  }
+  }
+
