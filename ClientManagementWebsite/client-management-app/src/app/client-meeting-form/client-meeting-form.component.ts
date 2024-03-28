731b7473cfaaa48908c509a53a583d37f7ea4e1b
Elena Favre <efavre@comcast.net>
2024-03-27 18:12:48 -0700

    added icon and updated forms

--- ClientManagementWebsite/client-management-app/src/app/client-meeting-form/client-meeting-form.component.ts
+++ ClientManagementWebsite/client-management-app/src/app/client-meeting-form/client-meeting-form.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-meeting-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './client-meeting-form.component.html',
   styleUrl: './client-meeting-form.component.css'
 })
 export class ClientMeetingFormComponent {
 
+  public meetingTopic = "";
+  public numberOfPeople = "";
+  public meetingDateTime = "";
+
+  onSubmit() {
+    console.log(`-----------------------------------------------`)
+    console.log(` Meeting Topic: ${this.meetingTopic}`)
+    console.log(`  Number of People: ${this.numberOfPeople}`)
+    console.log(` Meeting Date and Time: ${this.meetingDateTime}`)
+  }
 }
