d7b7d311f6a928e30a9ec389c2cda9c425e8af90
Elena Favre <efavre@comcast.net>
2024-03-28 19:46:56 -0700

    added testing files

---
+++ ClientManagementWebsite/client-management-app/src/test/resources/clientmanagementapptest/StepDefinitions.java
@@ -0,0 +1,8 @@
+package clientmanagementapptest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en Then;
+import static org.junit.jupiter.api.Assertions.*;
+
+class
---
+++ ClientManagementWebsite/client-management-app/src/test/resources/clientmanagementapptest/clientform.feature
@@ -0,0 +1,4 @@
+Feature: Message on entering invalid email address
+
+  Background:
+    Given
